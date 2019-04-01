b242f693540948b1aa6e9f52f134ffde6dfb8e18
monpeco <monpeco@gmail.com>
2019-04-01 18:54:07 +0000

    Add string properties and style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,20 @@ import React, {Component} from 'react';
 
 class Counter extends Component{
     state = {
-        count: 0
+        count: 0,
+        imageUrl: 'https://picsum.photos/200'
     };
+    
+    style = {
+        fontSize: 20
+    }
     
     render(){
         return(
             <React.Fragment>
-              <span>{this.formatCount()}</span>
-              <button>Increment</button>
+              <span style={ this.style } className="badge badge-primary m-2">{this.formatCount()}</span>  {/* apply class-name and style with an object */}
+              <button style={ { fontSize: 30} } className="btn btn-secundary btn-sm">Increment</button>  {/* apply class-name and style inline */}
+              <img src={this.state.imageUrl} alt=""/>
             </React.Fragment>
         );
     }
