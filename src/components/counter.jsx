5a71ae22dd7b59a95697ac85989cfd1b1b52e1ab
monpeco <monpeco@gmail.com>
2019-04-01 18:55:37 +0000

    Create helper method to add classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,13 +11,20 @@ class Counter extends Component{
     }
     
     render(){
+
         return(
             <React.Fragment>
-              <span style={ this.style } className="badge badge-primary m-2">{this.formatCount()}</span>  {/* apply class-name and style with an object */}
+              <span style={ this.style } className={this.getBadgeClasses()}>{this.formatCount()}</span>  {/* apply class-name and style with an object */}
               <button style={ { fontSize: 30} } className="btn btn-secundary btn-sm">Increment</button>  {/* apply class-name and style inline */}
               <img src={this.state.imageUrl} alt=""/>
             </React.Fragment>
         );
+    }
+    
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-"
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
     
     formatCount(){
