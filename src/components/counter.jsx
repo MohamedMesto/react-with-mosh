add04bee6a23a308a7971ff4f3ec5d667a7c3ce7
Ubuntu <ubuntu@ip-172-31-16-30.us-east-2.compute.internal>
2019-04-05 13:14:14 +0000

    Change state 'count' to 'value' - in Counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class Counter extends Component{
     state = {
-        count: this.props.value,
+        value: this.props.value,
         imageUrl: 'https://picsum.photos/200',
         tags: ['one','two','three'],
     };
@@ -18,7 +18,7 @@ class Counter extends Component{
     
     handlerIncrement = (id) => {  // Changing the function to a arrow function, 'this' is inherited
         console.log("Increment !!!", id);
-        this.setState({ count: this.state.count + 1});
+        this.setState({ value: this.state.value + 1});
 
     }
     
@@ -44,13 +44,13 @@ class Counter extends Component{
     
     getBadgeClasses(){
         let classes = "badge m-2 badge-"
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
     
     formatCount(){
-        const { count } = this.state;  // object destructuring
-        return count === 0 ? 'Zero' : count;
+        const { value } = this.state;  // object destructuring
+        return value === 0 ? 'Zero' : value;
     }
 }
 
