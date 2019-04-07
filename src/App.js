5a2cbd70b4dbb3da16ea007e5539f1e6559b9ef8
monpeco <monpeco@gmail.com>
2019-04-07 15:44:41 +0000

    Move counters array to App (make Counters a controlled component)

--- src/App.js
+++ src/App.js
@@ -4,12 +4,48 @@ import Counters from './components/counters'
 import './App.css';
 
 class App extends Component {
+  state = {
+      counters: [
+              { id: 1, value: 3 },
+              { id: 2, value: 0 },
+              { id: 3, value: 1 },
+              { id: 4, value: 0 },
+          ]
+  };
+  
+  handleIncrement = counter => {
+      const counters = [...this.state.counters];
+      const index = counters.indexOf(counter);
+      counters[index] = {...counter};
+      counters[index].value++;
+      this.setState({ counters });
+  }
+  
+  handleDelete = (counterId) => {
+      console.log("Event handler called", counterId);
+      const counters = this.state.counters.filter( count => count.id !== counterId);
+      this.setState({ counters });
+  };
+  
+  handleReset = () => {
+      console.log("handleReset");
+      const counters = this.state.counters.map(counter => {
+         counter.value = 0;
+         return counter;
+      });
+      this.setState({ counters });
+  }
+  
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
         <main className="container">
-          <Counters />
+          <Counters 
+          counters={this.state.counters}
+          onIncrement={this.handleIncrement} 
+          onDelete={this.handleDelete} 
+          onReset={this.handleReset} />
         </main>
       </React.Fragment>
     );
