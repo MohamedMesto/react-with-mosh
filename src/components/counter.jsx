1fc0e59d1e9040b5dfebcc14fee728b433258c26
monpeco <monpeco@gmail.com>
2019-04-07 12:05:51 +0000

    Remove Counter state (to convert it into a controlled component)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 
 class Counter extends Component{
-    state = {
-        value: this.props.counter.value,
-        //imageUrl: 'https://picsum.photos/200',
-        tags: ['one','two','three'],
-    };
+    // state = {
+    //     value: this.props.counter.value,
+    //     //imageUrl: 'https://picsum.photos/200',
+    //     tags: ['one','two','three'],
+    // };
     
     style = {
         fontSize: 20
@@ -16,11 +16,11 @@ class Counter extends Component{
     //     this.handlerIncrement = this.handlerIncrement.bind(this);
     // }
     
-    handlerIncrement = (id) => {  // Changing the function to a arrow function, 'this' is inherited
-        console.log("Increment !!!", id);
-        this.setState({ value: this.state.value + 1});
+    // handlerIncrement = (id) => {  // Changing the function to a arrow function, 'this' is inherited
+    //     console.log("Increment !!!", id);
+    //     this.setState({ value: this.state.value + 1});
 
-    }
+    // }
     
     render(){
         console.log(this.props);
@@ -28,30 +28,26 @@ class Counter extends Component{
             <div>
               {this.props.children}
               <span style={ this.style } className={this.getBadgeClasses()}>{this.formatCount()}</span>  {/* apply class-name and style with an object */}
-              <button onClick={ () => this.handlerIncrement({ id: 2 }) } style={ { fontSize: 30} } className="btn btn-secundary btn-sm">Increment</button>  {/* apply class-name and style inline */}
+              <button onClick={ () => this.props.onIncrement(this.props.counter) } style={ { fontSize: 30} } className="btn btn-secundary btn-sm">Increment</button>  {/* apply class-name and style inline */}
               <button onClick={ () => this.props.onDelete(this.props.counter.id) } className="btn btn-danger btn-sm m-2">delete</button>
-              <img src={this.state.imageUrl} alt=""/>
-              <div>{ this.state.tags.length === 0 && 'Please create a tag' }</div>
-              <ul>
-                { this.renderTags() }
-              </ul>
+
             </div>
         );
     }
     
-    renderTags(){
-        if(this.state.tags.length === 0) return <p>There are no tags</p>
-        return this.state.tags.map( tag => <li key={tag}>{tag}</li>)
-    }
+    // renderTags(){
+    //     if(this.state.tags.length === 0) return <p>There are no tags</p>
+    //     return this.state.tags.map( tag => <li key={tag}>{tag}</li>)
+    // }
     
     getBadgeClasses(){
         let classes = "badge m-2 badge-"
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
     
     formatCount(){
-        const { value } = this.state;  // object destructuring
+        const { value } = this.props.counter;  // object destructuring
         return value === 0 ? 'Zero' : value;
     }
 }
