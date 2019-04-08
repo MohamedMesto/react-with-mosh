9927ba8a32c439c6c12d14c845c1b9bd225bb8b2
monpeco <monpeco@gmail.com>
2019-04-08 20:42:45 +0000

    Fix to 3 decimal places the innerHTML value assigned on start

--- src/game/game.js
+++ src/game/game.js
@@ -3,7 +3,7 @@ var GF = () =>{
     var mainLoop = () => {
       // main function, called each frame  
       let element = document.getElementById('game');
-      element.innerHTML = Math.random();
+      element.innerHTML = Math.random().toFixed(3);
       requestID = requestAnimationFrame(mainLoop);
     };
     
